806a8e064963165b876a0933d320097930f56f51
mradul_09 <mradulnatani0@gmail.com>
2024-06-21 03:04:39 +0530

    added some initial info to the ui

---
+++ my-portfolio/src/MyComponents/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './main.css'
+export default function Main() {
+  return (
+    <div className='intro-content'>
+        <br/>
+        <br/>
+        <h3>Hello, I am Mradul Natani</h3>
+        <h3>I am a full stack web developer</h3>
+        <h3>also a D-app Developer</h3>
+    </div>
+  )
+}
---
+++ my-portfolio/src/MyComponents/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+export default function navbar() {
+  return (
+    <div>
+      <navbar className="navigation">
+        <ul>
+            <li>About</li>
+            <li>Technologies</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+      </navbar>
+    </div>
+  )
+}
