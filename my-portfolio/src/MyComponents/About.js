b60dd28d96230e31a4436139d1ca36dc7d6430b6
mradul_09 <mradulnatani0@gmail.com>
2024-06-28 01:20:45 +0530

    made changes in about component

--- my-portfolio/src/MyComponents/About.js
+++ my-portfolio/src/MyComponents/About.js
@@ -4,22 +4,22 @@ export default function About() {
     <div>
         <br/>
       <h1 style={{color:'yellow'}}><u style={{color:'yellow'}}>About Me</u></h1>
-      <pre style={{color:'white',fontSize:'15px', marginRight:'450px'}}>
-        <p style={{marginRight:'700px'}}>Hi there !</p>
-        <p>I am Mradul Natani, I am student of International Institute of Professional Studies, DAVV, Indore.</p>
-        <p>A driven and highly motivated individual with a strong belief in the power of smart and hard work </p>
-        <p>  ,passion and consistency. I do full stack web development and also I do decentralized app development</p>
-        <p>    using solidity programming language, I too have a strong hold on data structures and algorithms as well</p>
-        <p style={{marginRight:'550px'}}> a strong hold on concepts of OOPS.</p>
-        <p>    My journey in technology begun with facination towards coding which then resulted into a burning desire</p>
-        <p>     to learn more and more, Yes my desire to learn hass no limits !, also I use support of google, youtue and</p>
-        <p style={{marginRight:'550px'}}>AI tools to learn new concepts.</p>
-        <p>I am always open and ready to contribute with like-minded people, so that we can grow together.   </p><br/>
-        <b><p style={{marginRight:'700px'}}>Your sincerly,</p></b>
-        <b><p style={{marginRight:'700px'}}>Mradul Natani</p></b>
+      <pre style={{color:'white'}}>
+        <p>Hi there !</p>
+        I am Mradul Natani, I am student of International Institute of Professional Studies, DAVV, Indore. <br/>
+        A driven and highly motivated individual with a strong belief in the power of smart and hard work <br/>
+          ,passion and consistency. I do full stack web development and also I do decentralized app development <br/>
+          using solidity programming language, I too have a strong hold on data structures and algorithms as well <br/>
+          a strong hold on concepts of OOPS. <br/>
+           My journey in technology begun with facination towards coding which then resulted into a burning desire <br/>
+          to learn more and more, Yes my desire to learn hass no limits !, also I use support of google, youtue and <br/>
+        AI tools to learn new concepts. <br/>
+        I am always open and ready to contribute with like-minded people, so that we can grow together.  <br/> 
+        Your sincerly, <br/>
+        Mradul Natani
 
       </pre>
-     <button  style={{boxSizing:'border-box',border:'3px solid yellow',backgroundColor:'white',borderRadius:'5px',marginLeft:'10px',marginRight:'1160px'}}> <a href="https://www.dropbox.com/scl/fi/su3xntbwsycj81reb5ri7/MRADUL_NATANI.resume-1.docx?rlkey=zmijejs2wj35nw3h8yaavdiw1&st=p5age4uf&dl=0" download="MRADUL_NATANI.resume[1].docx" style={{backgroundColor:'white',fontSize:'15px',color:'black',letterSpacing:'2px'}}>Download CV</a></button>
+     <button  style={{boxSizing:'border-box',border:'3px solid yellow',backgroundColor:'white',borderRadius:'5px'}}> <a href="https://www.dropbox.com/scl/fi/su3xntbwsycj81reb5ri7/MRADUL_NATANI.resume-1.docx?rlkey=zmijejs2wj35nw3h8yaavdiw1&st=p5age4uf&dl=0" download="MRADUL_NATANI.resume[1].docx" style={{backgroundColor:'white',fontSize:'15px',color:'black',letterSpacing:'2px'}}>Download CV</a></button>
 
     </div>
   )
--- my-portfolio/src/MyComponents/technology.js
+++ my-portfolio/src/MyComponents/technology.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function technology() {
   return (
     <div>
-      <h1 style={{color:'yellow'}}>technology component</h1>
+      <h1 style={{color:'yellow'}}>Languages and technologies</h1>
+
     </div>
   )
 }
